652e4f3332e56a42fac0a27cba450adfc1fe2b89
Vitasenka <piroozamirpour@gmail.com>
2021-03-27 20:10:44 +0430

    add api

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -1,10 +1,10 @@
 // const sendinBlueApi = require("sib-api-v3-sdk");
 
 // const client = sendinBlueApi.ApiClient.instance();
+const { SENDINBLUE_API_KEY } = process.env;
 
 exports.handler = async function (event, context, callback) {
   console.log("hi");
-  const { SENDINBLUE_API_KEY } = process.env;
   return {
     statusCode: 200,
     body: JSON.stringify({ message: SENDINBLUE_API_KEY }),
