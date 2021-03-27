0de4cf602a0db1a81ba0a0bac12a69569462caad
Vitasenka <piroozamirpour@gmail.com>
2021-03-27 20:01:39 +0430

    add pricess.env

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -8,6 +8,6 @@ exports.handler = async function (event, context, callback) {
   // let data = JSON.parse(event.body);
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: process.env }),
   };
 };
