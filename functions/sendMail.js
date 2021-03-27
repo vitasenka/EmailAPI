8967aa1452370ff2cbf0b9fccb9e5603089bbc53
Vitasenka <piroozamirpour@gmail.com>
2021-03-27 21:30:53 +0430

    add new contact

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -1,9 +1,26 @@
-const sendinBlueApi = require("sib-api-v3-sdk");
-
-// const client = sendinBlueApi.ApiClient.instance();
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 const { SENDINBLUE_API_KEY } = process.env;
 
+var defaultClient = SibApiV3Sdk.ApiClient.instance;
+var apiKey = defaultClient.authentications["api-key"];
+apiKey.apiKey = SENDINBLUE_API_KEY;
+
+// const client = sendinBlueApi.ApiClient.instance();
+
 exports.handler = async function (event, context, callback) {
+  var apiInstance = new SibApiV3Sdk.ContactsApi();
+
+  var createContact = new SibApiV3Sdk.CreateContact(); // CreateContact | Values to create a contact
+  createContact = { email: "john@doe.com" };
+
+  apiInstance.createContact(createContact).then(
+    function (data) {
+      console.log("API called successfully. Returned data: " + data);
+    },
+    function (error) {
+      console.error(error);
+    }
+  );
   console.log("hi");
   return {
     statusCode: 200,
