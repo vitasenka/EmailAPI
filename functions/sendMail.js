dd6f9b8714b116b1a27733992aefdee5bec8ff33
Vitasenka <piroozamirpour@gmail.com>
2021-03-28 17:01:04 +0430

    fix instance error

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -33,7 +33,7 @@ const SibApiV3Sdk = require("sib-api-v3-sdk");
 
 exports.handler = async function (event, context) {
   const { SENDINBLUE_API_KEY } = process.env;
-  const defaultClient = SibApiV3Sdk.ApiClient.instance();
+  const defaultClient = SibApiV3Sdk.ApiClient.instance;
   var apiKey = defaultClient.authentications["api-key"];
   apiKey.apiKey = SENDINBLUE_API_KEY;
   var apiInstance = new SibApiV3Sdk.ContactsApi();
