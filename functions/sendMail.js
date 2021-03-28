bcc3e4fafd08ffcb310a67b55320fc3d0853685a
Vitasenka <piroozamirpour@gmail.com>
2021-03-28 16:41:11 +0430

    add contact

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -1,37 +1,52 @@
 const SibApiV3Sdk = require("sib-api-v3-sdk");
-const { SENDINBLUE_API_KEY } = process.env;
+// const SENDINBLUE_API_KEY =
+//   "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";
 
-var defaultClient = SibApiV3Sdk.ApiClient.instance;
-var apiKey = defaultClient.authentications["api-key"];
-apiKey.apiKey = SENDINBLUE_API_KEY;
+// // const client = sendinBlueApi.ApiClient.instance();
+// const handler = async function (event, context, callback) {
+//   var SibApiV3Sdk = require("sib-api-v3-sdk");
 
-// const client = sendinBlueApi.ApiClient.instance();
+//   var defaultClient = SibApiV3Sdk.ApiClient.instance;
 
-exports.handler = async function (event, context, callback) {
+//   // Configure API key authorization: api-key
+//   var apiKey = defaultClient.authentications["api-key"];
+//   apiKey.apiKey = SENDINBLUE_API_KEY;
+//   // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+//   //apiKey.apiKeyPrefix['api-key'] = "Token"
+
+//   var apiInstance = new SibApiV3Sdk.ContactsApi();
+
+//   var createContact = new SibApiV3Sdk.CreateContact(); // CreateContact | Values to create a contact
+//   createContact = { email: "john@doe.com" };
+
+//   apiInstance.createContact(createContact).then(
+//     function (data) {
+//       console.log("API called successfully. Returned data: " + data);
+//     },
+//     function (error) {
+//       console.error(error);
+//     }
+//   );
+// };
+
+// handler();
+
+exports.handler = async function (event, context) {
+  const { SENDINBLUE_API_KEY } = process.env;
+  const defaultClient = SibApiV3Sdk.ApiClient.instance();
+  var apiKey = defaultClient.authentications["api-key"];
+  apiKey.apiKey = SENDINBLUE_API_KEY;
   var apiInstance = new SibApiV3Sdk.ContactsApi();
 
-  var createContact = new SibApiV3Sdk.CreateContact(); // CreateContact | Values to create a contact
-  createContact = { email: "john@doe.com" };
+  var createContact = new SibApiV3Sdk.CreateContact();
+  createContact = { email: "johnWick@doe.com" };
 
-  // apiInstance.createContact(createContact).then(
-  //   function (data) {
-  //     console.log("API called successfully. Returned data: " + data);
-  //   },
-  //   function (error) {
-  //     console.error(error);
-  //   }
-  // );
-
-  var api = new SibApiV3Sdk.AccountApi();
-  api.getAccount().then(
+  apiInstance.createContact(createContact).then(
     function (data) {
-      return {
-        statusCode: 200,
-        body: JSON.stringify(data),
-      };
+      return "API called successfully. Returned data: " + data;
     },
     function (error) {
-      console.error(error);
+      return error;
     }
   );
 };
