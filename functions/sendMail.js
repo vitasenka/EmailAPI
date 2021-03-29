41713ce25ac102b6d25d98e4b4bd49f5119ddf6d
Vitasenka <piroozamirpour@gmail.com>
2021-03-29 23:23:13 +0430

    gmail transporter

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -1,37 +1,36 @@
-const SibApiV3Sdk = require("sib-api-v3-sdk");
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "witasenka@gmail.com",
+    pass: "860239387",
+  },
+});
+
+const message = {
+  from: "witasenka@gmail.com",
+  to: "piroozamirpour@gmail.com",
+  subject: "My firs email",
+  text: "Hi. I'm just testing my nodemailer function",
+};
+
+transporter
+  .sendMail(message)
+  .then((info) => {
+    console.log(JSON.stringify(info));
+  })
+  .catch((err) => {
+    console.log(JSON.stringify(err));
+  });
 
 exports.handler = function (event, context, callback) {
-  const { SENDINBLUE_API_KEY } = process.env;
-  const defaultClient = SibApiV3Sdk.ApiClient.instance;
-  var apiKey = defaultClient.authentications["api-key"];
-  apiKey.apiKey = SENDINBLUE_API_KEY;
-
-  var emailApi = new SibApiV3Sdk.TransactionalEmailsApi();
-
-  emailApi
-    .sendTransacEmail(
-      JSON.stringify({
-        subject: "My first Email",
-        sender: { name: "Pirooz", email: "piroozamirpour@gmail.com" },
-        replyTo: { name: "Pirooz", email: "piroozamirpour@gmail.com" },
-        to: { name: "victor", email: "victoramirpour@gmail.com" },
-        htmlContent:
-          "<html><body><h1>This is a transactional email {{params.bodyMessage}}</h1></body></html>",
-        params: { bodyMessage: "Made just for you!" },
-      })
-    )
-    .then(
-      function (data) {
-        callback(null, {
-          status: 200,
-          body: JSON.stringify({ message: data }),
-        });
-      },
-      function (error) {
-        console.log(JSON.stringify(error));
-        callback({
-          body: JSON.stringify({ message: error }),
-        });
-      }
-    );
+  transporter
+    .sendMail(message)
+    .then((info) => {
+      callback(null, JSON.stringify(info));
+    })
+    .catch((err) => {
+      callback(JSON.stringify(err));
+    });
 };
