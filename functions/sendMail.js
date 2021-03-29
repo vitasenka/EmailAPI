370fcbe62c6e36b662cf5637f5b74bcfaab83c46
Vitasenka <piroozamirpour@gmail.com>
2021-03-29 15:59:39 +0430

    test async 3

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -11,7 +11,7 @@ exports.handler = function (event, context, callback) {
     function (data) {
       callback(null, {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello World" }),
+        body: JSON.stringify({ message: data }),
       });
     },
     function (error) {
