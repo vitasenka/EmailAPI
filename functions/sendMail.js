1a9d934b2b014d9934db57a200afbe2b267bae04
Vitasenka <piroozamirpour@gmail.com>
2021-03-29 16:33:05 +0430

    test async 4

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -1,21 +1,15 @@
 const SibApiV3Sdk = require("sib-api-v3-sdk");
 
-exports.handler = function (event, context, callback) {
+exports.handler = async function (event, context, callback) {
   const { SENDINBLUE_API_KEY } = process.env;
   const defaultClient = SibApiV3Sdk.ApiClient.instance;
   var apiKey = defaultClient.authentications["api-key"];
   apiKey.apiKey = SENDINBLUE_API_KEY;
 
   var api = new SibApiV3Sdk.AccountApi();
-  api.getAccount().then(
-    function (data) {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({ message: data }),
-      });
-    },
-    function (error) {
-      console.error(error);
-    }
-  );
+  const data = await api.getAccount();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: data }),
+  };
 };
